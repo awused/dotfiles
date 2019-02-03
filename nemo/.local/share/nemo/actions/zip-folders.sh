06d16f84febf21dcdc1cd28f9285debcca426c9f
Desuwa <desuwa@desuwa.net>
2019-02-03 00:00:10 -0800

    Fix zip-folders after a nemo update broke it.

--- nemo/.local/share/nemo/actions/zip-folders.sh
+++ nemo/.local/share/nemo/actions/zip-folders.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 
-cd "$1"
 
-for dir in "${@:2}"; do
-  reldir=$(realpath --relative-to="$1" "$dir")
-  zip -r "$reldir" "$reldir" || notify-send "Failed to zip" "$reldir"
+for dir in "$@"; do
+  base=$(basename "$dir")
+
+  cd "$dir"
+
+  zip -r "../${base}.zip" "." || notify-send "Failed to zip" "$base"
 done
 
 notify-send "Done archiving"
