33574beb227a3e8774c97472452940166ab44ba6
Desuwa <desuwa@desuwa.net>
2019-07-24 22:36:47 -0700

    Improve mpv-anime screenshotter delegate

--- gui/.config/screenshotter/mpv-anime.sh
+++ gui/.config/screenshotter/mpv-anime.sh
@@ -4,9 +4,11 @@ set -e
 
 title=$(xprop -id $SCREENSHOTTER_WINDOWID WM_NAME)
 
-title=${title:19}
+beginning=$(echo $title | grep -b -o '= "' | cut -d: -f1 | head -n1)
+title=${title:($beginning + 3)}
 
-match=$(echo $title | grep -Po '^\[[^]]+\] \K(.*)(?= - [0-9]{2,})')
+match=$(echo $title | grep -Poi '^(\[[^]]+\] )?\K(.*)(?=( - [0-9]{2,})|( [e][0-9]{2,})|( - episode [0-9]+))')
+#match=$(echo $title | grep -Po '^\[[^]]+\] \K(.*)(?= - [0-9]{2,})()')
 
 echo "anime"
 echo "$match"
