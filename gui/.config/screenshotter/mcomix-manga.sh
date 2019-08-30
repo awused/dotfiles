044ae7ff097938ffed1e6a824e329df733e62153
Desuwa <desuwa@desuwa.net>
2019-08-30 02:42:01 -0700

    Screenshotter delegate for mcomix manga volumes.

---
+++ gui/.config/screenshotter/mcomix-manga.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+title=$(xprop -id $SCREENSHOTTER_WINDOWID WM_ICON_NAME)
+
+beginning=$(echo $title | grep -b -o '\]' | cut -d: -f1 | head -n1)
+title=${title:($beginning + 1)}
+
+match=$(echo $title | grep -Poi '^(\[[^]]+\] )?\K(.*)(?=( v[0-9]+))')
+
+echo "manga"
+echo "$match"
+
