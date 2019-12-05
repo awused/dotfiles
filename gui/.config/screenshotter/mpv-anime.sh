44c1b7b2a677650784aada71ba5895847eaf3a03
Desuwa <desuwa@desuwa.net>
2019-12-05 15:46:28 -0800

    Fix mpv-anime delegate.

--- gui/.config/screenshotter/mpv-anime.sh
+++ gui/.config/screenshotter/mpv-anime.sh
@@ -7,7 +7,7 @@ title=$(xprop -id $SCREENSHOTTER_WINDOWID WM_NAME)
 beginning=$(echo $title | grep -b -o '= "' | cut -d: -f1 | head -n1)
 title=${title:($beginning + 3)}
 
-match=$(echo $title | grep -Poi '^(\[[^]]+\] )?\K(.*)(?=( - [0-9]{2,})|( [e][0-9]{2,})|( - episode [0-9]+))')
+match=$(echo $title | grep -Poi '^(\[[^]]+\] )?\K(.*)(?=( - [0-9]{2,})|( [e][0-9]{2,})|( - episode [0-9]+)|( [0-9]{2,} - mpv))')
 #match=$(echo $title | grep -Po '^\[[^]]+\] \K(.*)(?= - [0-9]{2,})()')
 
 echo "anime"
