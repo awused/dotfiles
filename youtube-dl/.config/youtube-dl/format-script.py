a07ac387bf91faa5f061ea9e762879d2b3687504
Desuwa <desuwa@desuwa.net>
2019-12-27 14:45:49 -0800

    Fix youtube-dl video quality

---
+++ youtube-dl/.config/youtube-dl/format-script.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Script to generate preferred video formats.
+
+# Earlier entries are prioritized over later entriess
+qualifiers = [
+    # Resolution
+    ['[height>1440]', '[height>1080]', '[height=1080]', ''],
+    # FPS
+    ['[fps>30]', ''],
+    # Codec
+    ['[vcodec^=av01]', '[vcodec=vp9]', '']
+]
+
+
+def buildSet(prefix, i):
+    if i >= len(qualifiers):
+        return [prefix]
+
+    out = []
+    for f in qualifiers[i]:
+        out.extend(buildSet(prefix + f, i + 1))
+    return out
+
+
+formats = buildSet('', 0)
+
+print('--format \'', end='')
+for f in formats:
+    print('bestvideo{}+bestaudio/'.format(f), end='')
+
+print('best\'', end='')
