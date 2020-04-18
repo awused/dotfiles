9b1b8ca90ee7392cc4f4116ddb759a1ce70aec10
Desuwa <desuwa@desuwa.net>
2020-04-18 09:20:06 -0700

    Add mpv socket and ptscans delegate.

---
+++ gui/.config/screenshotter/firefox-ptscans.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+title=$(xprop -id $SCREENSHOTTER_WINDOWID WM_NAME)
+
+echo $title | grep -E 'ptscans\.com - Mozilla Firefox"$' > /dev/null
+
+beginning=$(echo $title | grep -b -o '= "' | cut -d: -f1 | head -n1)
+fulltitle=${title:($beginning + 3)}
+
+title=${fulltitle%% :: *}
+
+[ -z "$title" ] && exit 1
+
+# Exclude a few pages I might actually use
+# Don't try to be exhaustive
+
+echo "manga"
+echo "$title"
