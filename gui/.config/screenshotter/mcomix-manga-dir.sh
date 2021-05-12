52f8f74004c5bf514fdf05e6d44a3d943a0e0a52
Desuwa <desuwa@desuwa.net>
2021-05-11 22:08:19 -0700

    Fix screenshotter config when dealing with synced manga

--- gui/.config/screenshotter/mcomix-manga-dir.sh
+++ gui/.config/screenshotter/mcomix-manga-dir.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-cmd=$(ps -o cmd --pid $SCREENSHOTTER_PID | grep -E "/storage/media/manga/[^/]+/[^/]+\.zip")
-dir=$(echo "$cmd" | sed -E 's/^.*\/([^/]+)\/[^/]+\.zip$/\1/' | sed -E 's/ - [[:digit:]]+$//')
+cmd=$(ps -o cmd --pid $SCREENSHOTTER_PID | grep -E "/storage/media/manga/[^/]+($|/[^/]+\.zip)")
+dir=$(echo "$cmd" | sed -E 's/^.*\/manga\/([^/]+)($|\/[^/]+\.zip$)/\1/' | sed -E 's/ - [a-zA-Z0-9_-]+$//')
 
 [[ -z "$dir" ]] && exit 1
 
