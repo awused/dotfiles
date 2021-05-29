5ca2c56773eee648407cb7d0899a1928716f9d46
Desuwa <desuwa@desuwa.net>
2021-05-29 07:17:12 -0700

    Fix manga-dir delegate

--- gui/.config/screenshotter/mcomix-manga-dir.sh
+++ gui/.config/screenshotter/mcomix-manga-dir.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cmd=$(ps -o cmd --pid $SCREENSHOTTER_PID | grep -E "/storage/media/manga/[^/]+($|/[^/]+\.zip)")
+cmd=$(ps -ww -o cmd --pid $SCREENSHOTTER_PID | grep -E "/storage/media/manga/[^/]+($|/[^/]+\.zip)")
 dir=$(echo "$cmd" | sed -E 's/^.*\/manga\/([^/]+)($|\/[^/]+\.zip$)/\1/' | sed -E 's/ - [a-zA-Z0-9_-]+$//')
 
 [[ -z "$dir" ]] && exit 1
