e9a4b2c4033181b3ca833d36fb25def4ed708a60
Desuwa <desuwa@desuwa.net>
2021-05-29 17:52:39 -0700

    Dynasty scans support

---
+++ gui/.config/screenshotter/firefox-dynasty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+title=$(xprop -id $SCREENSHOTTER_WINDOWID WM_NAME)
+
+echo $title | grep -E '"Dynasty Reader » .* (-|—) Mozilla Firefox"$' > /dev/null
+
+beginning=$(echo $title | grep -b -o '= "' | cut -d: -f1 | head -n1)
+fulltitle=${title:($beginning + 20)}
+
+# Naively assume manga won't have hyphenated titles with hyphens surrounded by
+# spaces. This will be wrong at times but can be manually overridden
+title=$(echo $fulltitle | sed -E 's/ ch[0-9]+ (-|—) Mozilla Firefox"$//')
+
+[ -z "$title" ] && exit 1
+
+echo "manga"
+echo "$title"
