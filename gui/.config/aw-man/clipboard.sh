0e5e8d3279cbb2629dfedbd8144d21a0fd2c9759
Desuwa <desuwa@desuwa.net>
2024-11-06 10:44:30 -0800

    Work around more nvidia jank. Move to native clipboard for aw-man

--- gui/.config/aw-man/clipboard.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-# TODO -- improve this so it works more generally, needs to set the value for multiple targets.
-
-echo -en "copy\nfile://$AWMAN_CURRENT_FILE" | xclip -i -selection clipboard -t x-special/mate-copied-files
