eb46c2e9dc2d866d3997de957dece949bf902425
Desuwa <desuwa@desuwa.net>
2025-02-16 19:38:30 -0800

    Change my autosave script to maintain watch_later even while playing and periodically update it

--- mpv/.config/mpv/scripts/autosave.lua
+++ mpv/.config/mpv/scripts/autosave.lua
@@ -2,53 +2,50 @@
 --
 -- Adapted from https://gist.github.com/CyberShadow/2f71a97fb85ed42146f6d9f522bc34ef
 --
--- Saves a watch later config if the video has been paused for more than 1 minute.
---
--- Does not save in the last 30 seconds of the file.
+-- Saves a watch later config and updates it every 60 seconds and every time the video is paused for > 10 seconds.
 --
 -- Also clears watch later on clean exit.
 -- This lets you easily recover your position in the case of an ungraceful shutdown of mpv (crash, power failure, etc.).
-local save_delay = 60 * 1
+local save_period = 60 * 1
+local pause_delay = 10 * 1
 
 local mp = require 'mp'
 
-local save_timer = nil
-
-local function stop()
-  if save_timer ~= nil then
-    save_timer:stop()
-    save_timer = nil
-  end
-end
 
 local function save()
-  stop()
-
+  -- Do not save in the first 30 seconds of the file.
   -- local elapsed = mp.get_property_native("time-pos")
   --
   -- if elapsed < 30 then
   --   return
   -- end
 
-  local remaining = mp.get_property_native("time-remaining")
-
-  if remaining < 30 then
-    return
-  end
+  -- Do not save in the last 30 seconds of the file.
+  -- local remaining = mp.get_property_native("time-remaining")
+  --
+  -- if remaining < 30 then
+  --   mp.command("delete-watch-later-config")
+  --   return
+  -- end
 
   mp.commandv("set", "msg-level", "cplayer=warn")
   mp.command("write-watch-later-config")
   mp.commandv("set", "msg-level", "cplayer=status")
 end
 
+local pause_timer = mp.add_timeout(pause_delay, save, true)
+local save_period_timer = mp.add_periodic_timer(save_period, save)
+
 local function loaded()
   -- At least for my setup, starting paused almost always means this was resumed
   local paused = mp.get_property_native("pause")
 
-  if paused then
-    save()
-  else
-    stop()
+  save()
+  pause_timer:kill()
+  save_period_timer:kill()
+
+  if not paused then
+    save_period_timer:resume()
   end
 end
 
@@ -56,19 +53,18 @@ mp.register_event("file-loaded", loaded)
 
 local function pause(name, paused)
   if paused then
-    if save_timer == nil then
-      save_timer = mp.add_timeout(save_delay, save)
-    end
+    pause_timer:resume()
+    save_period_timer:stop()
   else
-    stop()
-    mp.command("delete-watch-later-config")
+    pause_timer:kill()
+    save_period_timer:resume()
   end
 end
 
 mp.observe_property("pause", "bool", pause)
 
 local function end_file(data)
-  stop()
+  pause_timer:kill()
 
   if data.reason == 'eof' or data.reason == 'stop' then
     local playlist = mp.get_property_native('playlist')
